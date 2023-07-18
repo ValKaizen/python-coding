7cc7eb8d774e9956414e73d42ee43698ed1e450d
ValKaizen <allurevalora@gmail.com>
2023-07-18 03:25:06 -0700

    Python script

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+output = ""
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i), end="")
-        print("{:02d}".format(j), end="\n" if i == 8 and j == 9 else ", ")
+        output += "{:02d}, {:02d}".format(i, j)
+        output += "\n" if i == 8 and j == 9 else ", "
+output = output[:179]  
+print(output)
+
+
+
+
+
+
+
